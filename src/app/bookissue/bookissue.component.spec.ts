ba7923a72955659d101121a32eac1e200fefa14b
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-24 21:01:55 +0530

    issue

---
+++ src/app/bookissue/bookissue.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookissueComponent } from './bookissue.component';
+
+describe('BookissueComponent', () => {
+  let component: BookissueComponent;
+  let fixture: ComponentFixture<BookissueComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BookissueComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BookissueComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
