1a48ac5fe77e5d5a68de2efcd8bbda12bb1745bc
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-09 07:39:53 +0530

    api

--- src/app/booksearch/booksearch.component.ts
+++ src/app/booksearch/booksearch.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-booksearch',
@@ -7,12 +8,43 @@ import { Component } from '@angular/core';
 })
 export class BooksearchComponent {
 
-  title=""
-
-  readValue=()=>
+  constructor(private api:ApiService){
+    api.fetchBook().subscribe(
+        (response:any)=>
+        {
+          this.data=response;
+        }
+      )
+    }
+    
+      data:any=[] 
+  
+  
+  name=""
+  searchBook:any=[]
+  readValues=()=>
   {
-    let data:any={"title":this.title}
+    let data:any={"name":this.name}
     console.log(data)
-  }
+    this.api.searchBook(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+        if(response.length==0){
+          alert("Invalid Book name")
+          this.name=""
+        }
+        else{
+          this.data=[];
+          this.searchBook=response;
 
+        }
+        
+          
+      }
+    )
+  
+  
+  }
+  
 }
